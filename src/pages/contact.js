93b94cdcdce59141951667c8607ffb876a029d5f
Edmund Lau <tmlcyaa@gmail.com>
2020-05-07 17:09:17 +0800

    added mailto html

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,8 +9,8 @@ const ContactPage = (props) => {
     <Layout>
       <Head title="Contact" />
       <h1>Contact</h1>
-      <p><strong>Reach me via email</strong> imedmundlcya@gmail.com</p>
-      <p>Reach me via <strong><a href="https://twitter.com/explore" target="_blank" rel="noopener noreferrer">Twitter</a></strong></p>
+      <p>Reach me via <strong><a href="mailto:imedmundlcy@gmail.com">email</a></strong></p>
+      <p>Or via <strong><a href="https://twitter.com/explore" target="_blank" rel="noopener noreferrer">Twitter</a></strong></p>
     </Layout>
   )
 }
