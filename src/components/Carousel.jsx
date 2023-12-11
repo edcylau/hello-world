d59938a70aaaeb21ad4f3236c6fe42ac5509d3a1
ed <tmlcyaa@gmail.com>
2023-12-11 11:30:34 +0000

    update browser view carousell:

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -24,49 +24,59 @@ export const Carousel = ({ images, section }) => {
       delay: 5000,
     },
   }
+// Style for the image container in BrowserView
+const browserImageContainerStyle = {
+  width: "100%", // Full width of the screen
+  paddingTop: "56.25%", // Maintains a 16:9 aspect ratio
+  position: "relative",
+}
 
-    // Style for the image container
-    const imageContainerStyle = {
-      width: "100%", // Full width of the screen
-    }
-  
-    // Style for images
-    const imageStyle = {
-      width: "100%",
-      height: "auto", // Maintain full height of the image
-      objectFit: "cover" // This will ensure the image covers the area, might crop if aspect ratio differs
-    }
-  
-    return (
-      <>
-        <BrowserView>
-          <Swiper {...swiperParams}>
-            {images && images.map((image, index) => {
-              const imageData = getImage(image);
-              return (
-                <div style={imageContainerStyle} key={`slide_${section}_${index}`}>
-                  <GatsbyImage style={imageStyle} loading="eager" image={imageData} alt={image.title} />
-                </div>
-              )
-            })}
-          </Swiper>
-        </BrowserView>
-        <MobileView>
-          <Swiper {...swiperParams}>
-            {images && images.map((image, index) => {
-              const imageData = getImage(image);
-              return (
-                <div style={imageContainerStyle} key={`slide_${section}_${index}`}>
-                  <GatsbyImage style={imageStyle} loading="eager" image={imageData} alt={image.title} />
-                </div>
-              )
-            })}
-          </Swiper>
-        </MobileView>
-      </>
-    )
-  }
-  
+// Style for images in BrowserView
+const browserImageStyle = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  objectFit: "cover", // This will ensure the image covers the area, might crop if aspect ratio differs
+}
+
+// Style for MobileView (if different styling is needed)
+const mobileImageStyle = {
+  width: "100%",
+  height: "auto",
+  objectFit: "cover",
+}
+
+return (
+  <>
+    <BrowserView>
+      <Swiper {...swiperParams}>
+        {images && images.map((image, index) => {
+          const imageData = getImage(image);
+          return (
+            <div style={browserImageContainerStyle} key={`slide_${section}_${index}`}>
+              <GatsbyImage style={browserImageStyle} loading="eager" image={imageData} alt={image.title} />
+            </div>
+          )
+        })}
+      </Swiper>
+    </BrowserView>
+    <MobileView>
+      <Swiper {...swiperParams}>
+        {images && images.map((image, index) => {
+          const imageData = getImage(image);
+          return (
+            <div style={{ width: "100%" }} key={`slide_${section}_${index}`}>
+              <GatsbyImage style={mobileImageStyle} loading="eager" image={imageData} alt={image.title} />
+            </div>
+          )
+        })}
+      </Swiper>
+    </MobileView>
+  </>
+)
+}
   Carousel.propTypes = {
     images: PropTypes.arrayOf(PropTypes.object).isRequired,
   }
